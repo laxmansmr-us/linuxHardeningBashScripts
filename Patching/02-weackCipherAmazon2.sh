49a2933ac6eb3f34c3c1ce8f5c6ca57797c0dc8d
Mohamed El Eraky <93321621+Mohamed-Eleraki@users.noreply.github.com>
2024-01-25 05:58:08 +0000

    remove senstive data

--- Patching/02-weackCipherAmazon2.sh
+++ Patching/02-weackCipherAmazon2.sh
@@ -7,7 +7,7 @@ if grep -i "Amazon linux" /etc/os-release; then
     echo "---------------------------------------"
     echo "The Machine OS is Amazon Linux, Will handel the subpolicy values and OpenSSH (CVE-2023-48795) by updating the OpenSSH Package"
 
-    yum update openssh >> /var/log/amazon/ssm/documents/hardening.log
+    yum update openssh >> /var/log/path/hardening.log
 
 else    
     CONTINUE=YES
