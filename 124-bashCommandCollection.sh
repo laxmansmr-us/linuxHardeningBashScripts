fff592719cb35805ec6f4aab1f5a797ef8bec9fa
Mohamed El Eraky <93321621+Mohamed-Eleraki@users.noreply.github.com>
2024-02-14 16:49:13 +0200

    load bashCommandCollection.sh

---
+++ 124-bashCommandCollection.sh
@@ -0,0 +1,22 @@
+echo "PROMPT_COMMAND='history -a >(logger -p local1.notice -t "[bash] $USER")'" >> /etc/bashrc
+
+printf "
+# command line audit logging
+local1.* -/var/log/commands.log"  >> /etc/rsyslog.conf
+
+systemctl restart --now rsyslog
+
+printf "
+/var/log/commands.log {
+    rotate 4
+    weekly
+    missingok
+    notifempty
+    compress
+    delaycompress
+    sharedscripts
+    postrotate
+        /usr/lib/rsyslog/rsyslog-rotate
+    endscript
+}
+" >> /etc/logrotate.d/bashCommandsCollection_SISA
